2a8804d60173af07ea645e22831d410b782f6057
Aryan Tyagi <aryan.tyagi@huulke.com>
2024-05-30 17:10:33 +0530

    updated cors

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", 'https://inventorypilot-backend.onrender.com'],
     credentials: true,
   })
 );
